ae64632a37f0c2faad58d6d61d0c0af439cb93f9
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-10-04 14:15:14 +0200

    Create route and a MyProfile component.

---
+++ src/components/MyProfile.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MyProfile = () => <div>Profile</div>;
+
+export default MyProfile;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Navbar = () => (
-  <div>Navbar</div>
-);
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/rockets',
+      text: 'Rockets',
+    },
+    {
+      id: 2,
+      path: '/missions',
+      text: 'Missions',
+    },
+    {
+      id: 3,
+      path: '/my-profile',
+      text: 'My profile',
+    },
+
+  ];
+  return (
+    <nav>
+      <ul>
+        {links.map((link) => (
+          <li key={link.id}>
+            <NavLink
+              to={link.path}
+              activeClassName="active-link"
+              exact
+            >
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
