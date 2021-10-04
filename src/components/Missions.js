51175802b9e86527e0d487f0ed416242334dac49
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-10-04 19:24:59 +0100

    Made some changes

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,22 +1,15 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
 import fetchMissions from '../helpers/fetchMissions';
 import { getMissions } from '../redux/missions/missions';
 import store from '../redux/storeConfig';
-import OneMission from './OneMission';
 
 const Missions = () => {
   useEffect(async () => {
     store.dispatch(getMissions(await fetchMissions()));
   }, []);
 
-  const missions = useSelector((state) => state.missions)
-  console.log(missions)
-
   return (
-    <div>
-      {console.log(missions)}
-    </div>
+    <div>Missions</div>
   );
 };
 
