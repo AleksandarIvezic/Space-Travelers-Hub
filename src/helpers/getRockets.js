6c34f71b76be1c0558068806a68c1636c49cb9fa
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-10-04 17:07:03 +0200

    Update getRocket to return selected data only

--- src/helpers/getRockets.js
+++ src/helpers/getRockets.js
@@ -1,7 +1,15 @@
 const getRockets = async () => {
   const request = await fetch('https://api.spacexdata.com/v3/rockets');
-  const result = request.json();
-  return result;
+  const result = await request.json();
+  const filteredResult = await result.map((rocket) => (
+    {
+      id: rocket.id,
+      name: rocket.rocket_name,
+      type: rocket.rocket_type,
+      flickr_images: rocket.flickr_images,
+    }
+  ));
+  return filteredResult;
 };
 
 export default getRockets;
