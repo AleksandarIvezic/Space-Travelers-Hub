f6e18f74c81c5256fa2f5aeef9ca0748200da9b3
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-10-04 14:08:50 +0100

    Add route for missions component

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,18 @@ import MyProfile from './MyProfile';
 
 import '../style/App.css';
 import Navbar from './Navbar';
+import Missions from './Missions';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Route path="/missions">
+        <Missions />
+      </Route>
       <Route path="/my-profile">
         <MyProfile />
       </Route>
-      <Route />
     </>
   );
 }
