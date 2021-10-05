c831061ef3a7ec22117b533c5afb4555aaa42ee3
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-10-05 18:42:32 +0200

    Display reserved rockets on MyProfile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,5 +1,36 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => <div>Profile</div>;
+const MyProfile = () => {
+  const rockets = useSelector((state) => state.rocketsReducer.rockets);
+  let reservedRockets;
+  if (rockets) {
+    reservedRockets = rockets.filter((rocket) => rocket.reserved);
+  }
+  return (
+    <div className="d-flex w-75 mx-auto">
+      <ul className="my-missions w-50" />
+      <div className="my-rockets w-50">
+        <h4>My Rockets</h4>
+        {reservedRockets && (
+          <ul className="my-rockets list-group ">
+            {reservedRockets.map((rocket) => (
+              <li
+                key={rocket.id}
+                className="list-group-item"
+              >
+                {rocket.name}
+              </li>
+            ))}
+          </ul>
+        )}
+        {(!reservedRockets || !reservedRockets[0]) && (
+          <h4 className="text-danger m-4">No reserved rockets! :(</h4>
+        )}
+
+      </div>
+    </div>
+  );
+};
 
 export default MyProfile;
