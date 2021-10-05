8b2a17c15750055adb938017e5f69b1af56fce46
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-10-05 17:02:21 +0100

    Adjust bootstrap table to accommodate buttons

--- src/components/OneMission.js
+++ src/components/OneMission.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { joinMission } from '../redux/missions/missions';
 import store from '../redux/storeConfig';
+import '../style/OneMission.css';
 
 const OneMission = (props) => {
   const { name, description, id } = props;
@@ -10,22 +11,27 @@ const OneMission = (props) => {
     store.dispatch(joinMission(e.target.id));
   };
 
+  const handleLeave = (e) => {
+    console.log(e.target.id)
+  }
+
   return (
     <>
       <th scope="row">{name}</th>
-      <td>{description}</td>
+      <td><div className="descri">{description}</div></td>
       <td>
         <span className="badge bg-secondary">NOT A MEMBER</span>
       </td>
       <td className="text-center">
         <button
           type="button"
-          className="btn btn-sm btn-outline-secondary"
+          className="btn btn-sm btn-outline-secondary mission-btn"
           onClick={handleJoin}
           id={id}
         >
-          JoinMission
+          Join Mission
         </button>
+        <button type="button" className="btn btn-sm btn-outline-danger mission-btn" onClick={handleLeave} id={id}>Leave Mission</button>
       </td>
     </>
   );
