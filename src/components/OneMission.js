d430947098f563a4cdf9c3afadb0cab5e82dbb3a
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-10-05 18:30:36 +0100

    Set reserved as default props

--- src/components/OneMission.js
+++ src/components/OneMission.js
@@ -4,16 +4,16 @@ import { joinMission, leaveMission } from '../redux/missions/missions';
 import store from '../redux/storeConfig';
 
 const OneMission = (props) => {
-  const { name, description, id, reserved } = props;
+  const {
+    name, description, id, reserved,
+  } = props;
 
   const handleJoin = (e) => {
     store.dispatch(joinMission(e.target.id));
-    console.log(reserved)
   };
 
   const handleLeave = (e) => {
     store.dispatch(leaveMission(e.target.id));
-    console.log(reserved)
   };
 
   return (
@@ -23,12 +23,12 @@ const OneMission = (props) => {
       <td>
         {reserved
           ? <span className="badge bg-info">Active Member</span>
-          : <span className="badge bg-secondary">NOT A MEMBER</span>
-        }
+          : <span className="badge bg-secondary">NOT A MEMBER</span>}
       </td>
       <td className="text-center">
         {!reserved
-          ? <button
+          ? (
+            <button
               type="button"
               className="btn btn-sm btn-outline-secondary mission-btn"
               onClick={handleJoin}
@@ -36,8 +36,8 @@ const OneMission = (props) => {
             >
               Join Mission
             </button>
-          : <button type="button" className="btn btn-sm btn-outline-danger mission-btn" onClick={handleLeave} id={id}>Leave Mission</button>
-        }
+          )
+          : <button type="button" className="btn btn-sm btn-outline-danger mission-btn" onClick={handleLeave} id={id}>Leave Mission</button>}
       </td>
     </>
   );
@@ -47,7 +47,11 @@ OneMission.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
-  reserved: PropTypes.bool
+  reserved: PropTypes.bool,
+};
+
+OneMission.defaultProps = {
+  reserved: false,
 };
 
 export default OneMission;
