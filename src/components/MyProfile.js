962ed5418286cb9e54adec03bf6f453956c9fb12
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-10-05 19:21:26 +0100

    Add reserved missions to MyProfile component

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -7,13 +7,30 @@ const MyProfile = () => {
   if (rockets) {
     reservedRockets = rockets.filter((rocket) => rocket.reserved);
   }
+
+  const missions = useSelector((state) => state.missions.missions)
+  let reservedMissions;
+  if (missions) {
+    reservedMissions = missions.filter((mission) => mission.reserved)
+  }
+
   return (
     <div className="d-flex w-75 mx-auto">
-      <ul className="my-missions w-50" />
-      <div className="my-rockets w-50">
+      <div className="my-missions w-50 me-2">
+        <h4>My Missions</h4>
+        {reservedMissions && (
+          <ul className="list-group">
+            {reservedMissions.map((reservedMission) => (
+              <li key={reservedMission.mission_id} className="list-group-item">{reservedMission.mission_name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+
+      <div className="my-rockets w-50 ms-2">
         <h4>My Rockets</h4>
         {reservedRockets && (
-          <ul className="my-rockets list-group ">
+          <ul className="my-rockets list-group">
             {reservedRockets.map((rocket) => (
               <li
                 key={rocket.id}
