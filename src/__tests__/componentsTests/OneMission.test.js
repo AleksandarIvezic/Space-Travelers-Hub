2e520c83317731b507c53d93137fe059605d9d86
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-10-06 18:25:30 +0200

    Update tests

--- src/__tests__/componentsTests/OneMission.test.js
+++ src/__tests__/componentsTests/OneMission.test.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { Provider } from 'react-redux';
-import {
-  render, screen, fireEvent,
-} from '@testing-library/react';
 import OneMission from '../../components/OneMission';
-import store from '../../redux/storeConfig';
 
 describe('OneMission', () => {
   it('renders correctly', () => {
@@ -18,24 +13,5 @@ describe('OneMission', () => {
       />)
       .toJSON();
     expect(tree).toMatchSnapshot();
-  });
-});
-
-describe('OneMission', () => {
-  test('joins mission', () => {
-    render(
-      <Provider store={store}>
-        <OneMission
-          reserved={false}
-          id="123"
-          name="name"
-          description="desc"
-        />
-      </Provider>,
-    );
-
-    fireEvent.click(screen.getByText('Join Mission'));
-    screen.debug();
-    expect(screen.getByText('Join Mission'));
   });
 });
