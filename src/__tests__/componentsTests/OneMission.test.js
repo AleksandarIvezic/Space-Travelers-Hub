b671224a1286280fbb5d52be2cfcdae902330696
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-10-06 15:56:54 +0100

    Begin test for OneMission events

--- src/__tests__/componentsTests/OneMission.test.js
+++ src/__tests__/componentsTests/OneMission.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import OneMission from '../../components/OneMission';
 
 describe('OneMission', () => {
@@ -15,3 +16,19 @@ describe('OneMission', () => {
     expect(tree).toMatchSnapshot();
   });
 });
+
+describe('OneMission', () => {
+  test('joins mission', () => {
+    render(<OneMission
+      reserved={false}
+      id="123"
+      name="name"
+      description="desc"
+      />);
+
+    const joinBtn = screen.getByText('Join Mission');
+    fireEvent.click(joinBtn)
+    screen.debug()
+    expect(screen.getByText('Leave Mission')).not.toBeInTheDocument()
+  })
+})
